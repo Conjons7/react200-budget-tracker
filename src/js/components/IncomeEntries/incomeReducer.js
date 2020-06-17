fa2670539852e145774b277ddf48557f22b3ce53
conor <conorjcunningham7@gmail.com>
2020-06-17 16:40:15 -0700

    finished project

---
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -0,0 +1,46 @@
+const defaultState = {
+    description: '',
+    amount: '',
+    lineItems: []
+};
+
+export default function IncomeReducer (state = defaultState, action) {
+
+    const { type, payload } = action;
+
+    switch (type) {
+        case 'UPDATE_INCOME_DECRIPTION': {
+            
+            return {
+
+                ...state,
+
+                description: payload.description
+            };
+        }
+
+        case 'UPDATE_INCOME_AMOUNT': {
+            return {
+                ...state,
+                amount: payload.amount
+            };
+        }
+
+        case 'ADD_INCOME': {
+            const { description, amount } = action.payload;
+            return {
+                description: '',
+                action: '',
+                lineItems: [
+                    ...state.lineItems,
+
+                    { description, amount }
+                ]
+            };
+        }
+
+        default: {
+            return state;
+        }
+    }
+}
